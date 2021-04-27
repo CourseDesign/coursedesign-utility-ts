7c367ac99c83f9108bb4d9006c6a1e9e94c15436
siyual-park <kdpark0723@gmail.com>
2021-04-28 00:25:20 +0900

    Change kebab-to-snake-case

--- packages/types/lib/change-case/kebab-to-snake-case.ts
+++ packages/types/lib/change-case/kebab-to-snake-case.ts
@@ -1,6 +1,7 @@
-import CamelToSnakeCase from "./camel-to-snake-case";
-import KebabToCamelCase from "./kebab-to-camel-case";
-
-type KebabToSnakeCase<S extends string> = CamelToSnakeCase<KebabToCamelCase<S>>;
+type KebabToSnakeCase<
+  S extends string
+> = S extends `${infer P1}-${infer P2}${infer P3}`
+  ? `${P1}_${P2}${KebabToSnakeCase<P3>}`
+  : S;
 
 export default KebabToSnakeCase;
