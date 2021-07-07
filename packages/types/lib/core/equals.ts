882de692415209add5d64c0748fbf2ce26344d5a
siyual-park <kdpark0723@gmail.com>
2021-07-07 11:35:07 +0900

    feat: rollback unused string

--- packages/types/lib/core/equals.ts
+++ packages/types/lib/core/equals.ts
@@ -1,4 +1,7 @@
-type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends
-  (<T>() => T extends Y ? 1 : 2) ? true : false;
+type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
 
 export default Equals;
